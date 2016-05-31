dc488e471658572085683470dd55bb61017e6d6e
Wojciech Gomoła <Wojciech Gomoa>
2016-05-31 19:04:26 +0200

    Initial commit

---
+++ src/Szogun1987.TaskExceptions/Szogun1987.TaskExceptions/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Szogun1987.TaskExceptions
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskExceptions;
+        }
+
+        private void TaskScheduler_UnobservedTaskExceptions(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            HandleException(e.Exception);
+        }
+        
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            HandleException(e.Exception);
+        }
+
+        private void HandleException(Exception ex)
+        {
+            var messageBuilder = new StringBuilder();
+            var exception = ex;
+            do
+            {
+                messageBuilder
+                    .Append("Message:")
+                    .AppendLine(exception.Message)
+                    .AppendLine("Stack trace: ")
+                    .Append(exception.StackTrace)
+                    .AppendLine();
+                exception = exception.InnerException;
+                if (exception != null)
+                {
+                    messageBuilder
+                        .Append("Caused by:")
+                        .AppendLine();
+                }
+            }
+            while (exception != null);
+
+            MessageBox.Show(messageBuilder.ToString());
+        }
+    }
+}
---
+++ src/Szogun1987.TaskExceptions/Szogun1987.TaskExceptions/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Szogun1987.TaskExceptions
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void WithAwait_Click(object sender, RoutedEventArgs e)
+        {
+            await ThrowException();
+        }
+
+        private void WithoutAwait_Click(object sender, RoutedEventArgs e)
+        {
+            ThrowException();
+        }
+
+        private Task ThrowException()
+        {
+            return Task.Run(() => { throw new Exception(); });
+        }
+
+        private void GCCollect_Click(object sender, RoutedEventArgs e)
+        {
+            GC.Collect();
+        }
+    }
+}
